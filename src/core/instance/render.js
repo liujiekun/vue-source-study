e0725b30f44e75d084938e64977332f51c910dff
liujiekun <liujiekun@everjiankang.com>
2020-05-21 00:12:24 +0800

    尝试使用命令操作git

--- src/core/instance/render.js
+++ src/core/instance/render.js
@@ -67,7 +67,6 @@ export function renderMixin (Vue: Class<Component>) {
   Vue.prototype.$nextTick = function (fn: Function) {
     return nextTick(fn, this)
   }
-
   Vue.prototype._render = function (): VNode {
     const vm: Component = this
     const { render, _parentVnode } = vm.$options
