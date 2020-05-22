f89bb98e6309215887e71be395406e07c6d7498b
liujiekun <liujiekun@everjiankang.com>
2020-05-22 19:12:54 +0800

    vue源码数组通过index方式赋值为什么不能动态响应

--- src/core/observer/index.js
+++ src/core/observer/index.js
@@ -24,7 +24,7 @@ const arrayKeys = Object.getOwnPropertyNames(arrayMethods)
  */
 export let shouldObserve: boolean = true
 
-export function toggleObserving(value: boolean) {
+export function toggleObserving (value: boolean) {
   shouldObserve = value
 }
 
@@ -63,7 +63,7 @@ export class Observer {
    * getter/setters. This method should only be called when
    * value type is Object.
    */
-  walk(obj: Object) {
+  walk (obj: Object) {
     const keys = Object.keys(obj)
     for (let i = 0; i < keys.length; i++) {
       defineReactive(obj, keys[i])
@@ -73,7 +73,8 @@ export class Observer {
   /**
    * Observe a list of Array items.
    */
-  observeArray(items: Array<any>) {
+  observeArray (items: Array<any>) {
+    // 看清楚了吧数组没有对每一项进行Object.defineProperty,而是直接对子项进行observe了
     for (let i = 0, l = items.length; i < l; i++) {
       observe(items[i])
     }
@@ -86,7 +87,7 @@ export class Observer {
  * Augment a target Object or Array by intercepting
  * the prototype chain using __proto__
  */
-function protoAugment(target, src: Object) {
+function protoAugment (target, src: Object) {
   /* eslint-disable no-proto */
   target.__proto__ = src
   /* eslint-enable no-proto */
@@ -97,7 +98,7 @@ function protoAugment(target, src: Object) {
  * hidden properties.
  */
 /* istanbul ignore next */
-function copyAugment(target: Object, src: Object, keys: Array<string>) {
+function copyAugment (target: Object, src: Object, keys: Array<string>) {
   for (let i = 0, l = keys.length; i < l; i++) {
     const key = keys[i]
     def(target, key, src[key])
@@ -109,7 +110,8 @@ function copyAugment(target: Object, src: Object, keys: Array<string>) {
  * returns the new observer if successfully observed,
  * or the existing observer if the value already has one.
  */
-export function observe(value: any, asRootData: ?boolean): Observer | void {
+export function observe (value: any, asRootData: ?boolean): Observer | void {
+  // 对象才有__ob__,字面量没有
   if (!isObject(value) || value instanceof VNode) {
     return
   }
@@ -134,7 +136,7 @@ export function observe(value: any, asRootData: ?boolean): Observer | void {
 /**
  * Define a reactive property on an Object.
  */
-export function defineReactive(
+export function defineReactive (
   obj: Object,
   key: string,
   val: any,
@@ -159,7 +161,7 @@ export function defineReactive(
   Object.defineProperty(obj, key, {
     enumerable: true,
     configurable: true,
-    get: function reactiveGetter() {
+    get: function reactiveGetter () {
       const value = getter ? getter.call(obj) : val
       if (Dep.target) { // watcher入栈
         dep.depend() // 收集依赖
@@ -172,7 +174,7 @@ export function defineReactive(
       }
       return value
     },
-    set: function reactiveSetter(newVal) {
+    set: function reactiveSetter (newVal) {
       const value = getter ? getter.call(obj) : val
       /* eslint-disable no-self-compare */
       if (newVal === value || (newVal !== newVal && value !== value)) {
@@ -200,7 +202,7 @@ export function defineReactive(
  * triggers change notification if the property doesn't
  * already exist.
  */
-export function set(target: Array<any> | Object, key: any, val: any): any {
+export function set (target: Array<any> | Object, key: any, val: any): any {
   if (process.env.NODE_ENV !== 'production' &&
     (isUndef(target) || isPrimitive(target))
   ) {
@@ -235,7 +237,7 @@ export function set(target: Array<any> | Object, key: any, val: any): any {
 /**
  * Delete a property and trigger change if necessary.
  */
-export function del(target: Array<any> | Object, key: any) {
+export function del (target: Array<any> | Object, key: any) {
   if (process.env.NODE_ENV !== 'production' &&
     (isUndef(target) || isPrimitive(target))
   ) {
@@ -267,7 +269,7 @@ export function del(target: Array<any> | Object, key: any) {
  * Collect dependencies on array elements when the array is touched, since
  * we cannot intercept array element access like property getters.
  */
-function dependArray(value: Array<any>) {
+function dependArray (value: Array<any>) {
   for (let e, i = 0, l = value.length; i < l; i++) {
     e = value[i]
     e && e.__ob__ && e.__ob__.dep.depend()
